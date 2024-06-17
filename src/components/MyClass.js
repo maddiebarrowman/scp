f152dab405ffa48af121e82d8203ddf3d73055e6
maddie <maddie.barrowman@gmail.com>
2024-06-17 21:23:23 +1200

    final final deploy

--- src/components/MyClass.js
+++ src/components/MyClass.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function MyClass() {
-  return <div className="title">Secure Contain Protect.</div>;
+  return (
+    <div className="container">
+      <h1>Welcome to the SCP Catalogue</h1>
+      <p>
+        Explore the Secure, Contain, Protect (SCP) Foundation's catalogue of anomalous objects, entities, and phenomena.
+      </p>
+      
+     
+    </div>
+  );
 }
 
 export default MyClass;
